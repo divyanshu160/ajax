152ef6f5ed9b7b21e97eee93e78a88cb486c8f26
Divyanshu <daseri@iitk.ac.in>
2021-01-11 17:07:02 +0530

    initial commit

---
+++ ajax.js
@@ -0,0 +1,29 @@
+
+const jokeDIV = document.querySelector('#joke');
+const button = document.querySelector('#get-joke');
+
+button.addEventListener('click', function(){
+    getRandomJoke();
+})
+
+function getRandomJoke(){
+    const ajax = new XMLHttpRequest;
+
+
+    ajax.open('GET', "https://api.chucknorris.io/jokes/random", true);
+
+    ajax.onreadystatechange = function(){
+        if(this.status === 200 && this.readyState === 4){
+            console.log(this.responseText);
+            let data = JSON.parse(this.responseText);
+            jokeDIV.innerHTML = `${data.value}`
+        } else {
+            this.onerror = onerror();
+        }
+    }
+    ajax.send();
+}
+
+function onerror(){
+     jokeDIV.textContent = 'There was an error!';  
+}
